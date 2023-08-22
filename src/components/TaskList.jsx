0a68ae99ec3e91e5c0ed409015f1b7187aaf9ce3
John Corn <jcornacchioli@gmail.com>
2023-08-22 15:55:08 -0400

    outline add button

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Task from './Task'
+import AddTask from './AddTask'
 
 function TaskList({tasks, completeTask, removeTask, editTask}) {
 
   return (
     <div className='w-full mx-auto'>
         <div className='flex flex-col gap-2 mx-auto'>    
-        <div className='bg-gray-500 h-100px w-100px mx-auto'>+</div>
+        <AddTask/>
             { tasks.map((task) => (
               <Task
               key={task.id}
